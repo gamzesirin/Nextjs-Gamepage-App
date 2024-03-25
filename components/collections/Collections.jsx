8bd48e4c2fba4726d60d434d3188f744989e7e55
Gamze Şirin <gamze1sirin@gmail.com>
2024-03-26 01:14:55 +0300

    kart sıralaması düzeltildi

--- components/collections/Collections.jsx
+++ components/collections/Collections.jsx
@@ -9,11 +9,11 @@ const Collections = () => {
 				<h1 className={styles.title}>Top Collection</h1>
 				<div className={styles.cards}>
 					<div className={styles.card}>
-						<Image src="/image/card1.svg" width={327} height={468} alt="card" />
+						<Image src="/image/card2.svg" width={327} height={468} alt="card" />
 					</div>
 
 					<div className={styles.card}>
-						<Image src="/image/card2.svg" width={327} height={468} alt="card" />
+						<Image src="/image/card1.svg" width={327} height={468} alt="card" />
 					</div>
 					<div className={styles.card}>
 						<Image src="/image/card3.svg" width={327} height={468} alt="card" />
