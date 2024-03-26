15243d96584d7855be547f3c6e7344f13373c722
Gamze Şirin <gamze1sirin@gmail.com>
2024-03-26 20:24:14 +0300

    deneme link

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Collections from '@/components/collections/Collections'
+import React from 'react'
+
+const page = () => {
+	return (
+		<div>
+			{' '}
+			<Collections />
+		</div>
+	)
+}
+
+export default page
